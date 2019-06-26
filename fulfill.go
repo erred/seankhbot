ad78faa8f1262ad6df5afa42460dbb4613208f46
Sean Liao <seankhliao@gmail.com>
2019-06-26 14:18:26 +0800

    more debugging

--- fulfill.go
+++ fulfill.go
@@ -7,7 +7,16 @@ import (
 
 // fulfill actually fulfills a WebhookRequest
 func (s *Server) fulfill(r *dfpb.WebhookRequest) (*dfpb.WebhookResponse, error) {
-	log.Debugln("fulfill started with", r)
+	log.Debugln("fulfill session", r.Session)
+	if r.QueryResult != nil {
+		log.Debugln("fulfill QR", r.QueryResult.Action, r.QueryResult.QueryText)
+		log.Debugf("fulfill QR intent %v\n", r.QueryResult.Intent)
+
+	}
+	if r.OriginalDetectIntentRequest != nil {
+		log.Debugln("fulfill ODIR", r.OriginalDetectIntentRequest.Source)
+		log.Debugf("fulfill ODIR payload %v\n", r.OriginalDetectIntentRequest.Payload)
+	}
 	res := &dfpb.WebhookResponse{
 		FulfillmentText: "I am fulfilled!!!",
 	}
