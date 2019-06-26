b857d720ddd53f4fe60ea23f412a1a23d90c939b
Sean Liao <seankhliao@gmail.com>
2019-06-26 12:48:22 +0800

    add more logging

--- fulfill.go
+++ fulfill.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	log "github.com/sirupsen/logrus"
 	dfpb "google.golang.org/genproto/googleapis/cloud/dialogflow/v2"
 )
 
 // fulfill actually fulfills a WebhookRequest
 func (s *Server) fulfill(r *dfpb.WebhookRequest) (*dfpb.WebhookResponse, error) {
+	log.Debugln("fulfill started with", r)
 	res := &dfpb.WebhookResponse{
 		FulfillmentText: "I am fulfilled!!!",
 	}
